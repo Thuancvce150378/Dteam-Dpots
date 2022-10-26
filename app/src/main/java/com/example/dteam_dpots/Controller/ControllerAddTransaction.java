bdbcf87fce14a95feb9bf13379fa89f504dfe3b0
thanhtuyenne <88424555+thanhtuyenne@users.noreply.github.com>
2022-10-26 21:38:30 +0700

    ui add transaction

---
+++ app/src/main/java/com/example/dteam_dpots/Controller/ControllerAddTransaction.java
@@ -0,0 +1,17 @@
+package com.example.dteam_dpots.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dteam_dpots.*;
+
+
+public class ControllerAddTransaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
